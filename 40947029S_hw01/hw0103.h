557953e6e78e94187e559deace67e5da08f24e10
Vonernue <seanho12345@gmail.com>
2021-03-05 11:37:04 +0800

    Upload example hw0103.h

---
+++ 40947029S_hw01/hw0103.h
@@ -0,0 +1,18 @@
+#ifndef HW0103_H
+#define HW0103_h
+
+#include <stdio.h>
+
+char *pStr01 = "Hello";
+char *pStr02 = "World";
+
+void print_answers()
+{
+    printf( "String 01:\n" );
+    printf( "%s\n", pStr01 );
+    printf( "String 02:\n" );
+    printf( "%s\n", pStr02 );
+    return;
+}
+
+#endif
