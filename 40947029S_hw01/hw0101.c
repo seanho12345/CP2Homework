44786e2928ac3e9348fab6c184d68a8c25e49d9e
Vonernue <seanho12345@gmail.com>
2021-03-12 17:50:49 +0800

    improved version add error detection.

--- 40947029S_hw01/hw0101.c
+++ 40947029S_hw01/hw0101.c
@@ -7,35 +7,30 @@ int main(){
     char *str = malloc(sizeof(char));
     printf("Please enter the hex string: ");
     char ch, *endptr;
-    int len = 0;
+    int len = 0,valid = 1;
+    char tmp[3]={0},count=0;
     while(ch != '\n'){
         ch = getchar();
-        str = realloc(str,sizeof(char) * len+1);
-        str[len] = ch;
-        len++;
-    }
-    str[len-1] = '\0';
-    //printf("%d\n",len);
-    char convert[(len-3)/2];
-    int convertlen = 0,valid = 1;;
-    for(int i=0;i<len-3;i+=2){
-        char tmp[2];
-        tmp[0] = str[i];
-        tmp[1] = str[i+1];
-        convert[convertlen] = strtol(tmp, &endptr, 16);
-        if(*endptr == '\0' || (convert[convertlen]<0 || convert[convertlen]>127)){
-            printf("ERROR\n");
+        tmp[count] = ch;
+        count++;
+        if(count == 2){
+            count = 0;
+            str[len] = strtol(tmp, &endptr, 16);
+            str = realloc(str,sizeof(char)*(len+1));
+            len++;
+        }
+        if(*endptr != '\0'){
+            printf("Error\n");
             valid = 0;
             break;
         }
-        convertlen++;
+    }
+    if(str[len-1] != '\0'){
+        valid = 0;
+        printf("Error\n");
     }
     if(valid == 1){
-        for(int i=0;i<convertlen;i++){
-            printf("%c",convert[i]);
-        }
-        printf("\n");
+        printf("%s\n",str);
     }
-    
     free(str);
 }
